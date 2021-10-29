e1e9a12747bac63287be045531fc86d64e389ca1
Sigurd Fosseng <sigurd@fosseng.net>
2021-10-29 23:06:00 +0200

    .

---
+++ hello-world-clj-http/core.clj
@@ -0,0 +1,44 @@
+(ns clojure.examples.hello
+  (:require
+   [clj-http.client :refer [get]]
+   [clj-http.conn-mgr :refer [make-reusable-conn-manager shutdown-manager]]
+   [clj-http.core :refer [build-http-client]])
+  (:gen-class))
+
+(defn get-some-json []
+  (-> "http://httpbin.org/get?my=arg&has=values"
+      (get {:as :json})
+      :body :args))
+
+(defn set-custom-useragent []
+  (-> "http://httpbin.org/get?my=arg&has=values"
+      (get {:headers {:User-Agent "Laat/clojure"}
+            :as :json})
+      :body :headers :User-Agent))
+
+(comment
+  (get-some-json)
+  (set-custom-useragent))
+
+;; connection manager for keep alive
+(def cm (make-reusable-conn-manager {:timeout 2 :threads 3}))
+(defn get-some-json-pooled []
+  (-> "http://httpbin.org/get"
+      (get {:as :json :connection-manager cm})
+      :body :headers))
+
+(comment
+  (get-some-json-pooled)
+  ;; Do I have to shutdown manually?
+  (-> (Runtime/getRuntime)
+      (.addShutdownHook (Thread. #(shutdown-manager cm)))))
+
+;; TODO: client-reuse for caching
+(def hclient (build-http-client {} false cm))
+(defn get-some-json-pooled-with-client-reuse []
+  (-> "http://httpbin.org/get"
+      (get {:as :json :connection-manager cm :http-client hclient})))
+
+(comment
+  (= hclient (:http-client (get-some-json-pooled-with-client-reuse)))
+  ,)
--- hello-world-http/core.clj
+++
@@ -1,8 +0,0 @@
-(ns clojure.examples.hello
-  (:require [clj-http.client :as client])
-  (:gen-class))
-
-(let [res (client/get "http://httpbin.org/get" {:accept :json :as :json})
-      body (:body res)
-      headers (:headers body)]
-  headers)
